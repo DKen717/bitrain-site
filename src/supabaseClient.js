1a584c7583887ba08328fb7adfe396d0317a7c3d
DKen717 <142824032+DKen717@users.noreply.github.com>
2025-05-26 16:11:50 +0500

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-// Замените эти значения на свои из Supabase → Settings → API
-const supabaseUrl = 'https://axtpugpwqyjadpbuqemi.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF4dHB1Z3B3cXlqYWRwYnVxZW1pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc3MzAzNzYsImV4cCI6MjA2MzMwNjM3Nn0.eoxkXa9aV27X-3be0jpvfDu9QylpeWC3C7Tcg9IPn3s'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(
+  'https://axtpugpwqyjadpbuqemi.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF4dHB1Z3B3cXlqYWRwYnVxZW1pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc3MzAzNzYsImV4cCI6MjA2MzMwNjM3Nn0.eoxkXa9aV27X-3be0jpvfDu9QylpeWC3C7Tcg9IPn3s'
+)

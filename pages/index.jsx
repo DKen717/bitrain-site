339e4812d42bcd12b7b0db901acfa2b3783e0d52
DKen717 <142824032+DKen717@users.noreply.github.com>
2025-05-27 17:18:11 +0500

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -16,6 +16,7 @@ const [filters, setFilters] = useState({
   toDate: today,
   selectedTimes: [],
   selectedWagons: [],
+  selectedTenants: [],
   workingStatus: '',
   minIdleDays: '',  // ➕ от
   maxIdleDays: ''   // ➕ до
@@ -44,6 +45,7 @@ const [filters, setFilters] = useState({
       toDate: '',
       selectedTimes: [],
       selectedWagons: [],
+      selectedTenants: [],
       workingStatus: '',
       minIdleDays: '',  // ➕ от
       maxIdleDays: ''   // ➕ до

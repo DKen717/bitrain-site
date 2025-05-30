a2e1991c4a2c5d277713b6b73235eddc560b5d7d
DKen717 <142824032+DKen717@users.noreply.github.com>
2025-05-30 17:37:28 +0500

    Update dashboard.jsx

--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -2,13 +2,13 @@ import { Box, Typography } from '@mui/material'
 
 export default function Dashboard() {
   return (
-        <>
-      <TopNav />
+  <>
+    <TopNav />
     <Box sx={{ padding: '2rem' }}>
       <Typography variant="h4">Дэшборд</Typography>
       <Typography>Здесь будут графики и ключевые показатели.</Typography>
     </Box>
-      </>
+  </>
   )
 }
 

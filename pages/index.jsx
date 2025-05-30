82294669d05ce9d60786a0b10bd3a20258a1b1a2
DKen717 <142824032+DKen717@users.noreply.github.com>
2025-05-30 17:25:58 +0500

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,18 +1,22 @@
 import Link from 'next/link'
+import TopNav from '../components/TopNav'
 import { Box, Typography, Button } from '@mui/material'
 
 export default function Home() {
   return (
-    <Box sx={{ padding: '2rem' }}>
-      <Typography variant="h4">Главная страница</Typography>
-      <Box sx={{ mt: 2 }}>
-        <Link href="/dislocation" passHref>
-          <Button variant="contained" sx={{ mr: 2 }}>Перейти в Дислокацию</Button>
-        </Link>
-        <Link href="/dashboard" passHref>
-          <Button variant="outlined">Перейти в Дэшборд</Button>
-        </Link>
+    <>
+      <TopNav />
+      <Box sx={{ padding: '2rem' }}>
+        <Typography variant="h4">Главная страница</Typography>
+        <Box sx={{ mt: 2 }}>
+          <Link href="/dislocation" passHref>
+            <Button variant="contained" sx={{ mr: 2 }}>Перейти в Дислокацию</Button>
+          </Link>
+          <Link href="/dashboard" passHref>
+            <Button variant="outlined">Перейти в Дэшборд</Button>
+          </Link>
+        </Box>
       </Box>
-    </Box>
+    </>
   )
 }

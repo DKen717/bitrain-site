6739b8f21f1bdff71a616b5cace05b2ca1696aec
DKen717 <142824032+DKen717@users.noreply.github.com>
2025-05-30 17:53:37 +0500

    Update TopNav.jsx

--- components/TopNav.jsx
+++ components/TopNav.jsx
@@ -1,18 +1,21 @@
-import { AppBar, Toolbar, Button } from '@mui/material'
+import { AppBar, Toolbar, Typography, Button } from '@mui/material'
 import Link from 'next/link'
 
 export default function TopNav() {
   return (
-    <AppBar position="static">
+    <AppBar position="static" sx={{ backgroundColor: '#f6d46b' }}>
       <Toolbar>
+        <Typography variant="h6" sx={{ flexGrow: 1, color: '#fff' }}>
+          Aiway Logistic
+        </Typography>
         <Link href="/" passHref>
-          <Button color="inherit">Главная</Button>
+          <Button sx={{ color: '#000000' }}>Главная</Button>
         </Link>
         <Link href="/dislocation" passHref>
-          <Button color="inherit">Дислокация</Button>
+          <Button sx={{ color: '#000000' }}>Дислокация</Button>
         </Link>
         <Link href="/dashboard" passHref>
-          <Button color="inherit">Дэшборд</Button>
+          <Button sx={{ color: '#000000' }}>Дэшборд</Button>
         </Link>
       </Toolbar>
     </AppBar>

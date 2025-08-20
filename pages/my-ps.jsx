3e34dc87d169ec59559f0fc9ccfe38ff45a23818
DKen717 <142824032+DKen717@users.noreply.github.com>
2025-08-20 12:47:36 +0500

    Update my-ps.jsx

--- pages/my-ps.jsx
+++ pages/my-ps.jsx
@@ -1,15 +1,18 @@
 // pages/my-ps.jsx
-import { useEffect, useState } from 'react'
-import { Container, Typography } from '@mui/material'
-import RentedParkTable from '../components/RentedParkTable'
+import { Typography } from '@mui/material'
+import dynamic from 'next/dynamic'
+import AppLayout from '../components/AppLayout'
+
+// если таблица 100% SSR-safe, можно убрать dynamic и импортировать напрямую
+const RentedParkTable = dynamic(() => import('../components/RentedParkTable'), { ssr: false })
 
 export default function MyPSPage() {
   return (
-    <Container>
-      <Typography variant="h4" gutterBottom>
+    <AppLayout>
+      <Typography variant="h4" sx={{ fontWeight: 800, mb: 2 }}>
         Мой ПС
       </Typography>
       <RentedParkTable />
-    </Container>
+    </AppLayout>
   )
 }

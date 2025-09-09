339737018e76881a0555281b90dbcd4a924fa0c1
DKen717 <142824032+DKen717@users.noreply.github.com>
2025-09-09 11:18:23 +0500

    Update arenda.jsx

--- pages/my-ps/arenda.jsx
+++ pages/my-ps/arenda.jsx
@@ -1,1 +1,18 @@
+// pages/my-ps.jsx
+import { Typography } from '@mui/material'
+import dynamic from 'next/dynamic'
+import AppLayout from '../components/AppLayout'
 
+// если таблица 100% SSR-safe, можно убрать dynamic и импортировать напрямую
+const RentedParkTable = dynamic(() => import('../components/RentedParkTable'), { ssr: false })
+
+export default function MyPSPage() {
+  return (
+    <AppLayout>
+      <Typography variant="h4" sx={{ fontWeight: 800, mb: 2 }}>
+        Мой ПС
+      </Typography>
+      <RentedParkTable />
+    </AppLayout>
+  )
+}

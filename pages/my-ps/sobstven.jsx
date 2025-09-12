d8ec9faa061add2ede15ad5f5bf24c4901ff0891
DKen717 <142824032+DKen717@users.noreply.github.com>
2025-09-12 15:30:26 +0500

    Update sobstven.jsx

--- pages/my-ps/sobstven.jsx
+++ pages/my-ps/sobstven.jsx
@@ -1,7 +1,6 @@
  import dynamic from 'next/dynamic'
  import AppLayout from '../../components/AppLayout'
  import { Container, Typography } from '@mui/material'
- import CompanyInfoBar from '../../components/CompanyInfoBar'
 
  const OwnedParkTable = dynamic(
    () => import('../../components/OwnedParkTable'),
@@ -15,7 +14,6 @@
          <Typography variant="h4" sx={{ fontWeight: 800, mb: 2 }}>
            Мой ПС — Вагоны в собственности
          </Typography>
-         <CompanyInfoBar />
          <OwnedParkTable />
        </Container>
      </AppLayout>
